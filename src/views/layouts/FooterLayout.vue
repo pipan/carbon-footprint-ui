527be2caee86c2ec940df9d07d856f9940260416
Peter Gasparik <gasparik.pe@gmail.com>
2021-05-27 22:37:43 +0200

    help article

--- src/views/layouts/FooterLayout.vue
+++ src/views/layouts/FooterLayout.vue
@@ -1,10 +1,11 @@
 <template>
-    <div class="column">
+    <div class="column flex">
         <div class="flex">
             <slot></slot>
         </div>
-        <footer>
-            <app-link class="btn btn--secondary" :route="{ name: 'article', params: { slug: 'about' } }">ABOUT</app-link>
+        <footer class="small">
+            <app-link class="btn btn--secondary" :route="{ name: 'article', params: { slug: 'about' } }">about</app-link>
+            <app-link class="btn btn--secondary" :route="{ name: 'article', params: { slug: 'help' } }">help</app-link>
         </footer>
     </div>
 </template>
